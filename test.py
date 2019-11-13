41beb559237e42f4dba91d8f806cbca4511cb424
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2019-11-13 10:28:31 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,65 @@
+import discord
+import diclog
+import math
+
+client = discord.Client()
+
+@client.event
+async def on_ready():#このbotがログインしたら
+    print(client.user.name+"がログインしました")#ターミナルにログインしたことを表示
+
+@client.event
+async def on_message(message):
+    if not message.author.bot:
+        if message.guild.id == 585998962050203672:
+            with open("./diclog.py","w") as f:
+                user_hatugensuu_dic = diclog.user_hatugensuu
+                try:
+                    before_hatugensuu = user_hatugensuu_dic[message.author.id]
+                    after_hatugensuu = before_hatugensuu + 1
+                except KeyError:
+                    after_hatugensuu = 1
+
+                if math.sqrt(after_hatugensuu) % 1 == 0:
+                    testchannel = client.get_channel(597978849476870153)
+                    await testchannel.send("ﾑﾑｯwwwﾚﾍﾞﾙｱｯﾌﾟwww\n"+message.author.name+"のレベル："+str(math.floor(math.sqrt(after_hatugensuu))))
+
+                user_hatugensuu_dic[message.author.id] = after_hatugensuu
+                f.write("user_hatugensuu = "+str(user_hatugensuu_dic))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.run("NTk4MzQ4MjQxMDI0NTgxNjU1.XSVVjQ.YULYcsLW2b4wWyoxx2bRDjb7waY")#botを動かすのに必要
