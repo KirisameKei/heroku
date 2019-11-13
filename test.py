c31dd856a5da8c97eb616fe127dd80c4771def32
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2019-11-13 14:55:56 +0900

    Update test.py

--- test.py
+++ test.py
@@ -28,6 +28,7 @@ async def on_message(message):
                 user_hatugensuu_dic[message.author.id] = after_hatugensuu
                 f.write("user_hatugensuu = "+str(user_hatugensuu_dic))
                 os.system("git commit")
+                os.system("git push")
 
 
 
