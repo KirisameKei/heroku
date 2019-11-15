f1e4a68156d0304717dda4a6c606aa187a75fd98
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2019-11-15 13:55:44 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ client = discord.Client()
 @client.event
 async def on_ready():#このbotがログインしたら
     print(client.user.name+"がログインしました")#ターミナルにログインしたことを表示
+    os.system("git config --global user.name "KirisameKei"")
+    os.system("git config --global user.email "hayabusa4013@gmail.com"")
+    os.system("git init")
 
 @client.event
 async def on_message(message):
@@ -27,10 +30,9 @@ async def on_message(message):
 
                 user_hatugensuu_dic[message.author.id] = after_hatugensuu
                 f.write("user_hatugensuu = "+str(user_hatugensuu_dic))
-                os.system("git add -u")
+                os.system("git add .")
                 os.system("git commit")
-                os.system("git remote add origin https://github.com/KirisameKei/discordbot.git")
-                os.system("git push origin master")
+                os.system("git push https://github.com/KirisameKei/discordbot origin master")
 
 
 
