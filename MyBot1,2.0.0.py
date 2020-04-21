374a5fcb78790f9387ddb4d6fd04c8f82f3202a9
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2020-04-22 04:15:00 +0900

    毎時0分に日間整地量を発表するように

--- MyBot1,2.0.0.py
+++ MyBot1,2.0.0.py
@@ -902,6 +902,9 @@ async def loop():
 
     if now_time.month == 5 and now_time.day == 6 and now_time.hour == 22 and now_time.minute == 0:
         await kei_ex_server.kikaku_choice(client1)
+        
+    if now_time.minute == 0:
+        await kyoutuu.daily_ranking(client1)
 
 
 loop.start()
