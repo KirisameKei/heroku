c8aa2386ee9a493e34e6571556df7483d4852bc6
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2020-11-19 10:17:04 +0900

    intentsについて記述

--- disboth.py
+++ disboth.py
@@ -18,7 +18,8 @@ import muhou
 import noname
 import kei_server
 
-client1 = discord.Client()
+intents = discord.Intents.all()
+client1 = discord.Client(intents=intents)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 discord_bot_token_1 = os.getenv("discord_bot_token_1")
